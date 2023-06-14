2fa78fac70a9bc9a1ca0df4aaf302d1ebc65e0f2
Anatoliy Liubich <a.liubich2000@gmail.com>
2023-06-14 17:30:24 +0300

    revork

--- src/Components/Conteiner/Conteiner.jsx
+++ src/Components/Conteiner/Conteiner.jsx
@@ -1,28 +1,25 @@
 import React from "react";
+
+import Input from "../Input/Input";
+
 import '../Conteiner/Conteiner.css'
+import '../Input/Input.css'
 
 class Conteiner extends React.Component {
    render () {
       return (
          <div className="content">
-            <div className="person_list">
-               <div className="avatar_conteiner">
-                  <img className="avatar" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Icecat1-300x300.svg/1024px-Icecat1-300x300.svg.png" alt="Avatar" />
-               </div>
-               <div className="main_info">
-                  <h2 className="main_info_item">Name:</h2>
-                  <p className="main_info_item">Date of Birth:</p>
-                  <p className="main_info_item">Education:</p>
-                  <p className="main_info_item">Web Site:</p>
-               </div>
+            <div className="photo_conteiner">
+               <img className="photo" src="https://cdn-icons-png.flaticon.com/512/300/300221.png" alt="Logo" />
+            </div>
+            <div className="nameList">
+               <h1>Name:</h1>
+               <p>Birthday:</p>
+               <p>Status:</p>
+               <p>Happy?</p>
             </div>
             <div className="lenta">
-               <h2>My Posts</h2>
-               <div className="message_button">
-                  <textarea className="input" type="text" placeholder="Write text" />
-                  <button className="message">Send</button>
-               </div>
-               <div>Any?</div>
+               <Input />
             </div>
          </div>
       )
---
+++ src/Components/Input/Input.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Input extends React.Component {
+   render () {
+      return (
+         <div className="input_conteiner">
+            <input placeholder="Write text" type="text" />
+         </div>
+      )
+   }
+}
+
+export default Input
